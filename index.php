292de8210a4dcbea8f1b3d7efce74cffec43caee
Deepak Ramachandran <deepudon077@gmail.com>
2016-06-09 20:27:11 +0530

    The main home page of the forums
    This is just the main home page of the forums

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<link href="stylesheets/index.css" type="text/CSS" rel="stylesheet"/>
+</head>
+<body>
+<div class="navigation_menu" >
+	<a href="#" ><div class="navigation_menu_button" ><br>Sign up</div></a>
+	<a href="#" ><div class="navigation_menu_button" ><br>Sign in</div></a>
+	<a href="#" ><div class="navigation_menu_button" ><br>Forums</div></a>
+	<a href="#" ><div class="navigation_menu_button" ><br>About</div></a>
+	<a href="#" ><div class="navigation_menu_button" ><br>Home</div></a>
+	<div class="navigation_menu_shortlogo" ><img src="images/crashflexlogo.png" width="300px" height="100px" ></div>
+</div>
+<div class="container_middle" >
+<div class="news" >
+	<div class="news_spacing" ></div>
+	<div class="news_titlebox" >News & Updates</div>
+	<div class="news_description" >->Introduced CrashFlex Forums into the site. (Deepak)<br>PS:CrashFlexForums was created by CrashFlex -_-</div>
+</div>
+<div class="right_infobar" >
+	<div class="right_infobar_info" ><br><br><br><br><br>Welcome To CrashFlex! Hope you have eyes to see our new project CrashFlexForums.org which helps people to create a forum-like website within minutes (even though you have no absolute knowledge about programming -_-)</div>
+</div>
+</div>
+<footer>Copyright &copy CrashFlexForums.org. All Rights Reserved.</footer>
+</body>
+</html>
